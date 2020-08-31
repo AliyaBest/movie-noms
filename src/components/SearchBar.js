6f966781db730b30fa1fa75a625e2d3aecc9d108
AliyaBest <aliyabest@gmail.com>
2020-08-30 22:53:06 -0400

    ADDED: input functionality to retrieve search terms

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React from "react";
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ""
+    };
 
-  render(){
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      searchTerm: evt.target.value
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.setState({
+      searchTerm: ""
+    });
+  }
+
+  render() {
     return (
       <div>
-        <form>
-          <input placeholder="Search Movies"></input>
+        <form id="searchForm" onSubmit={this.handleSubmit}>
+          <input
+            id="searchTerm"
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search Movies"
+            onChange={this.handleChange}
+          ></input>
           <button value="submit">SUBMIT</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default SearchBar
+export default SearchBar;
